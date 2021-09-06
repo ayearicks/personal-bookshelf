1b73e61d80d3c3d477ae3db6df7ddff3749cecaf
Ashley Yearicks <ashley.yearicks@gmail.com>
2021-09-06 16:23:06 -0400

    Initialize project

---
+++ functions/bookSearch/bookSearch.js
@@ -0,0 +1,10 @@
+const fetch = require('node-fetch')
+const axios = require(axios)
+
+const handler = async function () {
+  
+  const GOOGLE_BOOKS_API = process.env.GOOGLE_BOOKS_API
+
+}
+
+module.exports = { handler }
